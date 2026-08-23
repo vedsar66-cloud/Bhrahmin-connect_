b197aaf4fd1863ed9d7e9594125867759f2a9e6d
vedsar66-cloud <vedsar66@gmail.com>
2026-08-23 15:56:39 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { AuthProvider } from './hooks/useAuth';
+import { AuthProvider } from '../useAuth';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
