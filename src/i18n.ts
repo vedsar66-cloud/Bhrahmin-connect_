ebd9b04f62415e5638b348be18b00af7fea74867
vedsar66-cloud <vedsar66@gmail.com>
2026-08-23 15:43:09 +0530

    Initialize i18n with react-i18next configuration

---
+++ src/i18n.ts
@@ -0,0 +1,11 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(initReactI18next).init({
+  resources: { en: { translation: {} } },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false }
+});
+
+export default i18n;
