c59e24627fd964c7b9a7a551ef01f7e385a52c9a
vedsar66-cloud <vedsar66@gmail.com>
2026-08-23 15:27:52 +0530

    Remove unused i18n import from main.tsx
    Removed import statement for i18n module.

--- main.tsx
+++ main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import './i18n';
 import { AuthProvider } from './hooks/useAuth.tsx';
 
 createRoot(document.getElementById('root')!).render(
