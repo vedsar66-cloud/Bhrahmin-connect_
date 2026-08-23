b35844a6a80343fef44438e4d5f9c5eee7cd9b0e
vedsar66-cloud <vedsar66@gmail.com>
2026-08-23 16:07:54 +0530

    Create mockData.ts

---
+++ lib/mockData.ts
@@ -0,0 +1,6 @@
+export const INITIAL_DIRECTORY_MEMBERS = [];
+export const INITIAL_BLOOD_DONORS = [];
+export const INITIAL_BUSINESSES = [];
+export const INITIAL_ANNOUNCEMENTS = [];
+export const INITIAL_BLOOD_REQUESTS = [];
+
